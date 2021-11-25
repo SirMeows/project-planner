0babb6785b2df9c2559504ccbc62a1232f33ad88
ptsand <ptrsand@gmail.com>
2021-11-25 11:18:01 +0100

    initial commit

---
+++ src/test/java/dk/kea/projectplanner/ProjectPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.kea.projectplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectPlannerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
